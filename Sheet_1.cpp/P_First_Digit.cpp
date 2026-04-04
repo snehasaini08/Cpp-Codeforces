d23ace0b25150fd3b165ad503eaf9076bbb06f09
Sneha <snehasaini143786@gmail.com>
2026-04-04 11:29:16 +0530

    commit changes

--- Sheet_1.cpp/P_First_Digit.cpp
+++ Sheet_1.cpp/P_First_Digit.cpp
@@ -3,5 +3,11 @@ using namespace std;
 int main()
 {
     int X;
+    cin>>X;
+    X=X/1000;
+    if (X%2==0)
+    cout<<"EVEN"<<endl;
+    else
+    cout<<"ODD"<<endl;
     
 }
---
+++ Sheet_1.cpp/Q_Coordinates_of_a_point.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float X,Y;
+    cin>>X>>Y;
+    if(X>0 && Y>0)
+    cout<<"Q1";
+    else if(X<0 && Y>0)
+    cout<<"Q2";
+    else if(X<0 && Y<0)
+    cout<<"Q3";
+    else if(X>0 && Y<0)
+    cout<<"Q4";
+    else if(X==0 && Y==0)
+    cout<<"Origem";
+    else if(Y==0)
+    cout<<"Eixo X";
+    else if(X==0)
+    cout<<"Eixo Y";
+       
+}
