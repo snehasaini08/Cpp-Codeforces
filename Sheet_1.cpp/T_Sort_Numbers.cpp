c091b4c1084987576ef1c796baf5a39bedec8378
Sneha <snehasaini143786@gmail.com>
2026-04-06 23:31:36 +0530

    commit changes

--- Sheet_1.cpp/T_Sort_Numbers.cpp
+++ Sheet_1.cpp/T_Sort_Numbers.cpp
@@ -4,12 +4,42 @@ int main()
 {
     int A,B,C;
     cin>>A>>B>>C;
-    if(A>B && A>C && B>C)
+    
+    if(A>=B && B>=C){
+    cout<<C<<endl<<B<<endl<<A<<endl;
+    //cout<<endl<<A<<endl<<B<<endl<<C;
+    }
+
+    else if (A>=C && C>=B){
+    cout<<B<<endl<<C<<endl<<A<<endl;
+    //cout<<endl<<A<<endl<<B<<endl<<C;
+    }
+    
+
+    else if (B>=A && A>=C){
+    cout<<C<<endl<<A<<endl<<B<<endl;
+    //cout<<endl<<A<<endl<<B<<endl<<C;
+    }
+
+    else if (B>=C && C>=A){
+    cout<<A<<endl<<C<<endl<<B<<endl;
+    //cout<<endl<<A<<endl<<B<<endl<<C;
+    }
+
+    else if (C>=A && A>=B){
+    cout<<B<<endl<<A<<endl<<C<<endl;
+    //cout<<endl<<A<<endl<<B<<endl<<C;
+    }
+
+    else if(C>=B && B>=A) {
     cout<<A<<endl<<B<<endl<<C<<endl;
-    else if(B>A && B>C && C>A)
-    cout<<A<<endl<<B<<endl<<C<<endl;
-    else if(A>B && A>C && B>C)
-    cout<<A<<endl<<B<<endl<<C<<endl;
+    //cout<<endl<<A<<endl<<B<<endl<<C;
+    }
+
+    cout<<endl<<A<<endl<<B<<endl<<C;
+
+
+
 
 }
 
